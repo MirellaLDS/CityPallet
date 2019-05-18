6410b73ad8eb02f5f7ef0c8ab315e84d41a1e645
MirellaLins <mirella.lds@gmail.com>
2019-05-18 14:29:39 -0300

    Update version gradler and api

--- pokedex/app/src/main/java/com/example/mirella/pokedex/Item2Activity.java
+++ pokedex/app/src/main/java/com/example/mirella/pokedex/Item2Activity.java
@@ -8,7 +8,6 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v4.media.session.ParcelableVolumeInfo;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
